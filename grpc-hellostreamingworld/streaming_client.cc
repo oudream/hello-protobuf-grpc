9f4ca6be13d878fb69ad2eef1babdab5491c3d93
oudream <oudream@126.com>
2018-07-23 10:03:39 +0800

    grpc steaming hello complete
    grpc steaming hello complete

--- grpc-hellostreamingworld/streaming_client.cc
+++ grpc-hellostreamingworld/streaming_client.cc
@@ -23,64 +23,68 @@
 #include <grpcpp/grpcpp.h>
 
 #ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
+#include "examples/protos/hellostreamingworld.grpc.pb.h"
 #else
-#include "helloworld.grpc.pb.h"
+#include "hellostreamingworld.grpc.pb.h"
 #endif
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
+using hellostreamingworld::HelloRequest;
+using hellostreamingworld::HelloReply;
+using hellostreamingworld::MultiGreeter;
 
 class GreeterClient {
- public:
-  GreeterClient(std::shared_ptr<Channel> channel)
-      : stub_(Greeter::NewStub(channel)) {}
+public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(MultiGreeter::NewStub(channel))
+    {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    void sayHello(const std::string &user)
+    {
+        // Data we are sending to the server.
+        HelloRequest request;
+        request.set_name(user);
 
-    // Container for the data we expect from the server.
-    HelloReply reply;
+        // Container for the data we expect from the server.
+        HelloReply reply;
 
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
 
-    // The actual RPC.
-    Status status = stub_->SayHello(&context, request, &reply);
+        // The actual RPC.
+        std::unique_ptr<::grpc::ClientReader<::hellostreamingworld::HelloReply>> upReader =
+            stub_->sayHello(&context, request);
 
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
+        // Act upon its status.
+        int iCount = 0;
+        while (upReader->Read(&reply))
+        {
+
+            std::cout << "reply index : " << iCount << ", message: " << reply.message() << std::endl;
+            ++ iCount;
+        }
     }
-  }
 
- private:
-  std::unique_ptr<Greeter::Stub> stub_;
+private:
+    std::unique_ptr<MultiGreeter::Stub> stub_;
+
 };
 
-int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  GreeterClient greeter(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  std::cout << "Greeter received: " << reply << std::endl;
+int main(int argc, char **argv)
+{
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint (in this case,
+    // localhost at port 50051). We indicate that the channel isn't authenticated
+    // (use of InsecureChannelCredentials()).
+    GreeterClient greeter(grpc::CreateChannel(
+        "localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    greeter.sayHello(user);
 
-  return 0;
+    return 0;
 }
--- grpc-hellostreamingworld/streaming_server.cc
+++ grpc-hellostreamingworld/streaming_server.cc
@@ -16,9 +16,41 @@
  *
  */
 
+//stl
+#include <typeinfo>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <bitset>
+#include <stack>
+#include <queue>
 #include <iostream>
-#include <memory>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <limits>
+
+//clib
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <iso646.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+#include <wctype.h>
 
 #include <grpcpp/grpcpp.h>
 
@@ -28,6 +60,7 @@
 #include "hellostreamingworld.grpc.pb.h"
 #endif
 
+using namespace std;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -38,35 +71,48 @@ using hellostreamingworld::MultiGreeter;
 
 // Logic and data behind the server's behavior.
 class GreeterServiceImpl final : public MultiGreeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
+    virtual ::grpc::Status
+    sayHello(::grpc::ServerContext *context, const ::hellostreamingworld::HelloRequest *request, ::grpc::ServerWriter<::hellostreamingworld::HelloReply> *writer) override
+//    Status SayHello(ServerContext* context, const HelloRequest* request,
+//                  HelloReply* reply) override
+    {
+        std::string prefix("Hello ");
+        for (int i = 0; i < 10; ++i)
+        {
+            HelloReply reply;
+            stringstream ss;
+            ss << i;
+            reply.set_message("Hello-" + ss.str());
+            writer->Write(reply);
+        }
+//        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
 
-int main(int argc, char** argv) {
-  RunServer();
+int main(int argc, char **argv)
+{
+    RunServer();
 
-  return 0;
+    return 0;
 }
